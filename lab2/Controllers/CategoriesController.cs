829fadff872129fc676eccac68278de7efbf0370
mehulshah1610 <36570956+mehulshah1610@users.noreply.github.com>
2019-01-26 16:52:01 -0500

    Lab2 completed

---
+++ lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(String Categories)
+        {
+            @ViewBag.Categories = "You selected: " + Categories;
+            return View();
+
+
+        }
+    }
+}
